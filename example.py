43ed29f9f48ab6f85a30dce797e8d96d0c91a8a3
Dan Berrebbi <dan.berrebbi@polytechnique.edu>
2022-11-08 11:15:39 -0500

    hparams

--- example.py
+++ example.py
@@ -10,17 +10,30 @@ bs=1
 init_dim = 64
 hidden_dim_gat = 128
 output_dim_gat = 128
+ff_dim = 128
+n_heads = 1
+emb_dim = 128
+dimacs_directory = './dimacs_files/test_small'
+n_transformer_layers = 1
+dropout = 0.1
 
 
-model = GATCodeur(n_layers=3, in_features=init_dim, n_hidden=hidden_dim_gat, ff_dim=128, n_heads=1, emb_dim=128, qk_dim=128, v_dim=128, dropout=0.1)
-
+model = GATCodeur(n_layers=n_transformer_layers,
+                  in_features=init_dim,
+                  n_hidden=hidden_dim_gat,
+                  ff_dim=ff_dim,
+                  n_heads=n_heads,
+                  emb_dim=emb_dim,
+                  qk_dim=emb_dim,
+                  v_dim=emb_dim,
+                  dropout=dropout)
 
 # loss and optimization  # TODO : see if GAT has a particular optimization
 criterion = MSELoss()
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=0.0005, betas=(0.9, 0.99))
 
-dimacs_directory = './dimacs_files/test_small'
+
 constraints, objective = read_dimacs_directory(dimacs_directory)
 
 train_model(constraints, objective, model, optimizer, criterion, bs)
