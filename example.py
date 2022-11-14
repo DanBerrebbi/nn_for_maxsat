c905f93dff630c5ff0ba92d6e351658d14884755
Dan Georges Berrebbi <dberrebb@tir-0-3.eth>
2022-11-14 10:32:48 -0500

    change initial embeddings

--- example.py
+++ example.py
@@ -7,15 +7,15 @@ from torch.nn import MSELoss, L1Loss
 
 
 bs=1
-init_dim = 64
+init_dim = 2
 hidden_dim_gat = 128
 output_dim_gat = 128
 ff_dim = 128
 n_heads = 1
 emb_dim = 128
 dimacs_directory = './dimacs_files/test_small'
-n_transformer_layers = 4
-dropout = 0.1
+n_transformer_layers = 0
+dropout = 0.0
 
 
 model = GATCodeur(n_layers=n_transformer_layers,
@@ -31,9 +31,9 @@ model = GATCodeur(n_layers=n_transformer_layers,
 # loss and optimization  # TODO : see if GAT has a particular optimization
 criterion = MSELoss()
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=0.0005, betas=(0.9, 0.99))
-
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.001, betas=(0.9, 0.98))
+#optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
 constraints, objective = read_dimacs_directory(dimacs_directory)
 
-train_model(constraints, objective, model, optimizer, criterion, bs)
+train_model([constraints[0]], objective, model, optimizer, criterion, bs)
--- train.py
+++ train.py
@@ -6,19 +6,29 @@ import numpy as np
 import torch
 
 
-def constraints_to_graph(constraints):
-  G = nx.Graph()
-  for i,clause in enumerate(constraints[0]):
-    G.add_nodes_from(clause)
-    G.add_node("c_{}".format(i))
-    for node in clause:
-      G.add_edge(node,"c_{}".format(i))
+def constraints_to_graph(constraints, directed=True):
+  if directed:
+    G = nx.DiGraph()
+    for i,clause in enumerate(constraints[0]):
+      for node in clause:
+        if node == abs(node):   # only one node for x_1 and -x_1
+          G.add_edge(node,"c_{}".format(i))
+        else :
+          G.add_edge("c_{}".format(i), abs(node))
+    return G
+  else:
+    G = nx.Graph()
+    for i,clause in enumerate(constraints[0]):
+      G.add_nodes_from(clause)
+      G.add_node("c_{}".format(i))
+      for node in clause:
+        G.add_edge(node,"c_{}".format(i))
 
-  # add hard constraint # TODO see how to handle those edges, different type ?
-  hard_c = get_hard_constraints(constraints[0])
-  for c in hard_c:
-      G.add_edge(c[0],c[1])
-  return G
+    # add hard constraint # TODO see how to handle those edges, different type ?
+    hard_c = get_hard_constraints(constraints[0])
+    for c in hard_c:
+        G.add_edge(c[0],c[1])
+    return G
 
 
 
@@ -86,52 +96,61 @@ def train_model(constraints, objectives, model, optimizer, criterion, bs):
     # enumerate epochs
     for epoch in range(20):
         print("--- Epoch {} ---".format(epoch))
-        for i, constraint in enumerate(constraints):
+        for I, constraint in enumerate(constraints):
             optimizer.zero_grad()
-
+            #import pdb;pdb.set_trace()
             # get initial embeddings from node2Vec (no gradient here) # TODO : no more Node2Vec --> degree would be better IMO
             nb_nodes = n_nodes_from_constraints(constraint)
             graph = constraints_to_graph(constraint)
             liste_nodes = list(graph.nodes)
             adj_mat = torch.tensor(np.array([nx.to_numpy_matrix(graph)]))
-            #import pdb;pdb.set_trace()
+            
             nodes_init_embeddings = []  # TODO : replace by one-hot encodings with degree if litteral (or 0 if clause)
-            for i , (node,degree) in enumerate(list(dict(graph.degree).items())):
-                if "c" in str(node) :
-                    nodes_init_embeddings.append(0)
+            for i , (node, degree) in enumerate(list(dict(graph.in_degree).items())):   # needs directed graphs ! 
+                if False and "c" in str(node) :
+                    nodes_init_embeddings.append([-1,-1])   # maybe not, maybe we should embed them as "normal nodes"
                 else :
-                    nodes_init_embeddings.append(degree)
-            nodes_init_embeddings=torch.nn.functional.one_hot(torch.tensor(nodes_init_embeddings), num_classes=model.init_dim)
-
-
+                    nodes_init_embeddings.append([float(degree), float(graph.out_degree[node])])
+            nodes_init_embeddings=torch.tensor(nodes_init_embeddings)
+            #nodes_init_embeddings=torch.rand(len(liste_nodes), 64)
+            #import pdb;pdb.set_trace()
 
             # compute the model output
-            logits = model(nodes_init_embeddings[0], adj_mat.permute(1, 2, 0))
-
+            #logits = model(nodes_init_embeddings, adj_mat.permute(1, 2, 0))
+            logits = model(nodes_init_embeddings, adj_mat)
+            
             # add projection/summation head to get the number of satisfied assignments
             # trick
-            temp = 0.001
+            #import pdb; pdb.set_trace()
+            temp = 0.1
             sftm = torch.nn.functional.softmax(logits[0] / temp, -1)
-            sat = torch.sum(torch.max(sftm[:, 0][constraint_to_ids(constraint, liste_nodes)], -1).values,
+            # need to duplicate sftm so that there is the negatives. Probably smarter way to do it
+            litteral_lines=[]
+            for ii,node in enumerate(liste_nodes):
+              if type(node)==int:
+                litteral_lines.append(ii)
+            liste_nodes_litterals = [liste_nodes[x] for x in litteral_lines]
+            liste_nodes_litterals_full = [x for x in liste_nodes_litterals]
+            for jj,lit in enumerate(liste_nodes_litterals):
+              liste_nodes_litterals_full.append(-abs(lit))
+            sftm_lit = sftm[litteral_lines]
+            
+            sftm_lit_bar = torch.ones_like(sftm_lit, requires_grad=True) - sftm_lit
+            sftm_lit_full = torch.cat((sftm_lit, sftm_lit_bar), 0)
+            
+            sat = torch.sum(torch.max(sftm_lit_full[:, 0][constraint_to_ids(constraint, liste_nodes_litterals_full)], -1).values,   # ici faut faire attention a si c'est un moins ou un plus
                             dtype=torch.float32)
-
+            
             # calculate loss
-            targets = objectives[i]  # TODO get real !! Check
-            loss = criterion(sat, targets)
-            # add hard constraints :
-            hard_constraints = constraint_to_ids([list(get_hard_constraints(constraint[0]))], liste_nodes)
-            hard_loss = 0
-            for c in hard_constraints:
-                hard_loss += criterion(torch.tensor(1.0), sftm[c[0], 1] + sftm[c[1], 1])
-
-            loss += 10 * hard_loss
+            targets = objectives[I]  # TODO get real !! Check
+            loss = 10*criterion(sat, targets)
+            # no need for additional loss for hard constraints here !
             loss.backward()
             optimizer.step()
 
-            if i % 10 == 0 :
-                print("loss :", loss.item())
-                print("broken : ", get_broken(hard_constraints, sftm), "   sat : ", sat.item())
-    print("broken : ", get_broken(hard_constraints, sftm), "   sat : ", sat)
+            if I % 1 == 0 :
+                print("loss :", loss.item(), "   sat : ", sat.item())
+    print("sat : ", sat)
     # print(sftm)
 
 
